c6deea09dee45e85838ede98b24176f099f91608
Immortalbug <stephenvicson@gmail.com>
2021-08-06 13:05:49 +0200

    Group Controller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GroupRequest;
+use App\Http\Resources\GroupCollection;
+use App\Models\Group;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class GroupController extends Controller
 {
@@ -13,18 +17,26 @@ class GroupController extends Controller
      */
     public function index()
     {
-        //
+        return new GroupCollection(Group::paginate());
     }
-
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(GroupRequest $request)
     {
-        //
+        $input = $request->all();
+
+        $group = Group::create($input);
+        // $group->setStatus('active');
+
+        $group = Group::find($group->id);
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $group->addMediaFromRequest('image')->toMediaCollection('group');
+        }
+        return response()->json(['status' => 200, 'data' => $group]);
     }
 
     /**
@@ -33,9 +45,11 @@ class GroupController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+
+    public function show(Group $group)
+
     {
-        //
+        return $group->getFirstMedia('group')->getUrl('cover');
     }
 
     /**
@@ -45,9 +59,11 @@ class GroupController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(GroupRequest $request, $id)
     {
-        //
+        $group = Group::find($id);
+        $group->update($request->all());
+        return $group;
     }
 
     /**
@@ -58,6 +74,6 @@ class GroupController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Group::destroy($id);
     }
 }
